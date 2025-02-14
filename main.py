6495bc6a3c95497c0335a5769e2abc2781606073
Samarth Bhandavale <sambhandavale15@gmail.com>
2025-02-14 13:52:43 +0530

    Add home page

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 import io
 import sys
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 @app.route('/python',methods=['POST'])
 def run_python_code():
